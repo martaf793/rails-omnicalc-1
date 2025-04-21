aaaf105656b61e9145741c5ae72e0a9f65421ec0
martaf793 <martaf@uchicago.edu>
2025-04-21 21:34:04 +0000

    random

--- app/controllers/calculators_controller.rb
+++ app/controllers/calculators_controller.rb
@@ -13,9 +13,18 @@ class CalculatorsController < ApplicationController
   def square_root_new
     render ({:template => "calc_templates/square_root_new"})
   end
-  def square_roots_results
+  def square_root_results
     @users_n=params.fetch("users_n")
     @result=@users_n.to_f**0.5.to_f
     render ({:template => "calc_templates/square_root_results"})
+  end
+  def random_new
+    render ({:template => "calc_templates/random_new"})
+  end
+  def random_results
+    @users_n1=params.fetch("users_n1").to_f
+    @users_n2=params.fetch("users_n2").to_f
+    @result=rand(@users_n1..@users_n2)
+    render ({:template => "calc_templates/random_results"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   get("/square/new", {:controller=> "calculators", :action=> "square_new"})
   get("/square/results", {:controller => "calculators", :action => "square_results"})
   get("/square_root/new", {:controller => "calculators", :action => "square_root_new"})
-  get("/square_root/results", {:controller => "calculators", :action => "square_roots_results"})
-  # get("/random/new", {:controller => "calculators", :action => "random_new"})
-  # get("/random/results", {:controller => "calculators", :action => "random_results"})
+  get("/square_root/results", {:controller => "calculators", :action => "square_root_results"})
+  get("/random/new", {:controller => "calculators", :action => "random_new"})
+  get("/random/results", {:controller => "calculators", :action => "random_results"})
   # get("/payment/new", {:controller => "calculators", :action => "payment/results"})
   # get("/payment/results", {:controller => "calculators", :action => "payment_results"})
 end
